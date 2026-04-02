d6e82b114c1f6dfe59938091ff11d4c67465bbc2
Rianne Kramer <rianjusss@gmail.com>
2026-04-02 15:45:02 +0200

    Added multiple pages for the client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { GameComponent } from './pages/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'game', component: GameComponent },
+  { path: '**', redirectTo: 'home' },
+];
---
+++ src/app/pages/game/game.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  imports: [],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.css',
+})
+export class GameComponent {}
