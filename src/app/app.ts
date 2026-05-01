223f7e968f6c5fd0a6960428c8661e85b6bbd681
Rianne Kramer <715731@student.inholland.nl>
2026-05-01 09:22:49 +0200

    Add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, inject, signal } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
 import { LoginStore } from './features/auth/data-access/login.store';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('set-game-client');
